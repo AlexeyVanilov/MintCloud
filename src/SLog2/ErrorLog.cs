90b114cb693552c420d4e8a4eceabc041e3cd771
cl1nzet <divashofizov@gmail.com>
2026-04-17 11:42:55 +0500

    added log system

---
+++ src/SLog2/ErrorLog.cs
@@ -0,0 +1,8 @@
+﻿namespace CommandSystem.Logs {
+    /// <summary>
+    /// All error logs
+    /// </summary>
+    public static class ErrorLog {
+        public const string nullCommandName = "Command name cannot be null!";
+    }
+}
