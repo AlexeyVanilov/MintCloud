1c8599df5d20f146a379fd997f6c225fb0f634b1
cl1nzet <divashofizov@gmail.com>
2026-04-22 19:51:23 +0500

    refactor(Utils): StringUtils.Dispatch

--- src/Utils/StringUtils.cs
+++ src/Utils/StringUtils.cs
@@ -3,9 +3,31 @@
     /// Static-class helper
     /// </summary>
     public static class StringUtils {
-        public static string[] Dispatch(string input) =>
-            input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        public static string[] Dispatch(string input, char separator = ' ') {
+            if (string.IsNullOrWhiteSpace(input)) return Array.Empty<string>();
 
+            ReadOnlySpan<char> span = input.AsSpan().Trim();
+
+            int wordCount = 0;
+            foreach (var range in span.Split(separator))
+            {
+                if (!span[range].IsWhiteSpace()) wordCount++;
+            }
+
+            string[] output = new string[wordCount];
+            int index = 0;
+
+            foreach (var range in span.Split(separator))
+            {
+                var word = span[range];
+                if (!word.IsWhiteSpace())
+                {
+                    output[index++] = word.ToString();
+                }
+            }
+
+            return output;
+        }
         public static string[] Slice(string[] parts)
         {
             if (parts.Length <= 1) return Array.Empty<string>();
